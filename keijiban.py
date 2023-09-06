b4243b71f0fad6cd537ad0cea5e027bb0481d79b
miyamiko <taka3mel@mj.scn-net.ne.jp>
2023-09-07 07:58:48 +0900

    ブログリンク変更

--- keijiban.py
+++ keijiban.py
@@ -9,7 +9,7 @@ from datetime import datetime
 st.title('掲示板　-　イチゲブログ')
 st.caption('最下部の入力欄に書いてEnterしてください。削除も可能です。')
 st.markdown('###### Streamelitやこのサイトの関連情報は')
-link = '[イチゲブログ](https://kikuichige.com/17180/)'
+link = '[イチゲブログ](https://kikuichige.com/21772/)'
 
 prompt=st.chat_input("何か書いてEnterまたは右のボタンをクリック！")
 st.markdown(link, unsafe_allow_html=True)
@@ -22,29 +22,6 @@ for i in range(kosuu):
     st.write(str(df.index[i]),'番')
     st.write('    日付:',df.iat[i,0])
     st.write('    内容:',df.iat[i,1])
-    # sen='<hr size="60">'
-    # st.write(sen, unsafe_allow_html=True)
-#**********************************************
-# print('for前'+str(kosuu))
-#削除を押したあとに実行される下のfor文でkosuuが削除された分少なく更新されない
-#理由はわからないがpassでエラーを無視した
-# try:
-#     for i in range(kosuu):
-#         # print('for後'+str(kosuu))
-#         if st.button("削除", key=i):
-#             st.write(str(df.index[i]),'番削除')
-#             df1=df.drop(i, axis=0)
-#             df=df1
-#             df.to_csv("toukou.csv")
-#             kosuu-=1
-#         st.write(str(df.index[i]),'番')
-#         st.write('    日付:',df.iat[i,0])
-#         st.write('    内容:',df.iat[i,1])
-#         sen='<hr size="6">'
-#         st.write(sen, unsafe_allow_html=True)
-# except:
-#     pass
-#**********************************************
 
 if prompt:
 # 水平線を表示
@@ -58,11 +35,8 @@ if prompt:
     df_append = pd.DataFrame(data=list, columns=columns)
     df1 = pd.concat([df, df_append], ignore_index=True, axis=0)
     df=df1
-    # df=df_append
     df.to_csv("toukou.csv")
-    # text='<span style="color:blue">表示更新を押してください！</span>'
-    # st.write(text, unsafe_allow_html=True)
-#**********************************************
+
 # 水平線を表示
 st.markdown("<hr>", unsafe_allow_html=True)
 with st.form(key='keijiban_form'):
@@ -78,8 +52,6 @@ with st.form(key='keijiban_form'):
         df.to_csv("toukou.csv")
         text='<span style="color:red">表示更新を押してください！</span>'
         st.write(text, unsafe_allow_html=True)
-# st.dataframe(df)
-
 
 # カスタムCSSスタイルを定義
 custom_css = """
@@ -128,71 +100,3 @@ if uploaded_file is not None:
     df7.to_csv("toukou.csv")
 # 水平線を表示
 st.markdown("<hr>", unsafe_allow_html=True)
-
-
-
-    # x=df['内容'].count()
-    # kosuu=int(x)
-    # for i in range(kosuu):
-    #     if st.button("削除", key=i):
-    #         st.write('削除',str(i))
-    #     st.write(str(i),'番')
-    #     st.write('    日付:',df.iat[i,0])
-    #     st.write('    内容:',df.iat[i,1])
-#**********************************************
-        # text='<span style="color:blue">df.iat[i,1]</span>'
-        # st.write(text, unsafe_allow_html=True)
-# else:
-#     df1=df
-# del_list=df.index.values
-
-# st.title('簡易掲示板イチゲブログ')
-# st.caption('csvファイルを使って掲示板作ってみました。')
-# with st.form(key='keijiban_form'):
-#     name=st.text_input('名前')
-#     message=st.text_input('メッセージ')
-#     st.text('投稿を押したあと表示更新を押してください。')
-#     toukou_btn=st.form_submit_button('投稿')
-#     hyouji_btn=st.form_submit_button('表示更新')
-#     st.markdown('###### 簡易掲示板')
-#     # del_no=st.text_input('削除する番号を選んでください')
-#     #セレクトボックス
-#     df=pd.read_csv("toukou.csv",index_col=0)
-#     st.dataframe(df)
-#     del_list=df.index.values
-#     del_no=st.selectbox(
-#             '削除する番号を選んでください',
-#             (del_list))
-#     # hyouji_btn=st.form_submit_button('削除直後や、セレクトボックスの値がおかしいときは、このボタンを押してください。セレクトボックスの番号がリフレッシュされます。')
-#     del_btn=st.form_submit_button('削除')
-#     if toukou_btn or hyouji_btn:
-#         # df=pd.read_csv("toukou.csv",index_col=0)
-#         if toukou_btn: 
-#             dt_now = datetime.now()
-#             toukoubi=dt_now.strftime('%Y年%m月%d日 %H:%M:%S')
-#             columns = [ '投稿日','名前','内容']
-#             list = [[toukoubi,name,message
-#             ]]
-#             df_append = pd.DataFrame(data=list, columns=columns)
-#             df1 = pd.concat([df, df_append], ignore_index=True, axis=0)
-#             df=df1
-#             df.to_csv("toukou.csv")
-#             text='<span style="color:blue">表示更新を押してください！</span>'
-#             st.write(text, unsafe_allow_html=True)
-#         else:
-#             df1=df
-#         # df1
-#         del_list=df.index.values
-#         # del_no=st.selectbox(
-#         #         '削除する番号を選んでください',
-#         #         (del_list))
-#     if del_btn:
-#         # st.text('del_no'+del_no)
-#         # df=pd.read_csv("toukou.csv",index_col=0)
-#         df1=df.drop(del_no, axis=0)
-#         # df1
-#         df=df1
-#         del_list=df.index.values
-#         df.to_csv("toukou.csv")
-#         text='<span style="color:red">表示更新を押してください！</span>'
-#         st.write(text, unsafe_allow_html=True)
